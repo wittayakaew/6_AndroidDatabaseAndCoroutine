83acd4dc7e6aaeee0ed53256af0cadbc919bc84c
wittayakaew <59160039@go.buu.ac.th>
2019-10-29 17:00:11 +0700

    create dao

--- src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -22,6 +22,16 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "daily_sleep_quality_table")
 data class  SleepNight(
+        @PrimaryKey(autoGenerate = true)
+        var nightId: Long = 0L,
 
+        @ColumnInfo(name = "start_time_milli")
+        val startTimeMilli: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "end_time_milli")
+        var endTimeMilli: Long = startTimeMilli,
+
+        @ColumnInfo(name = "quality_rating")
+        var sleepQuality: Int = -1
 
 )
